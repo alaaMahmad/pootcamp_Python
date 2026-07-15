4bbae70836a5fc5f3cc43f886503250b2962788f
Alaa Ahmad <alaa056945@gmail.com>
2026-07-15 19:27:54 +0300

    Make Hello World Assignment

---
+++ first_disscussion/hello_world.py
@@ -0,0 +1,21 @@
+#1
+print("Hello World")
+
+#2a
+name = "Alaa"
+print("Hello", name + "!") 
+#2b
+print("Hello " + name + "!")  
+
+# 3a
+num = 5
+print("Hello",num,"!")  
+# 3b
+print("Hello " + str(num) + "!") 
+
+# 4a
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1, fave_food2)) 
+# 4b
+print(f"I love to eat {fave_food1} and {fave_food2}.")            
